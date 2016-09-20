3480e69681ee070009696122e33b60fc3f911fae
Arnar Birgisson <arnar@Arnars-Air.lan>
2016-09-20 23:43:16 +0000

    Bætti við sliding menu

---
+++ side-nav.js
@@ -0,0 +1,39 @@
+'use strict';
+
+function setUpSideNavigation() {
+  let menuButton = document.querySelector('#menu-button');
+  let sections = document.querySelectorAll('section, footer');
+  let sideNav = document.querySelector('aside#side-nav');
+  let sectionsVisible = true;
+  let isInTransition = false;
+  menuButton.addEventListener('click', function() {
+    if (isInTransition) return;
+    isInTransition = !isInTransition;
+    toggleMenu();
+    setTimeout(function() {
+      isInTransition = !isInTransition;
+    }, 1100);
+  });
+
+  function toggleMenu() {
+    if (sectionsVisible) {
+      setTimeout(toggleHiddenClass, 600, sections);
+    } else {
+      toggleHiddenClass(sections);
+    }
+    setTimeout(function () {
+      for (let i = 0; i < sections.length; i++) {
+        sections.item(i).classList.toggle('transform-off-screen-left');
+      }
+    }, 30);
+    sideNav.classList.toggle('visible');
+    sectionsVisible = !sectionsVisible;
+  }
+  function toggleHiddenClass(elements) {
+    for (let i = 0; i < elements.length; i++) {
+      elements.item(i).classList.toggle('hide');
+    }
+  }
+}
+
+setUpSideNavigation();
